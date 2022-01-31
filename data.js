2532e556548c70d20264eff72e782beb00ace49e
Tamim94 <golam.tamim94@gmail.com>
2022-01-31 16:00:03 +0100

    commit

---
+++ data.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'MUSIC/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'MUSIC/img/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'MUSIC/img/cover 2.png'
+    },
+    // +6 more
+]
